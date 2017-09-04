85b331fff4136fa0ecacccf3e58ebaca08d36a21
SOMYUUKTHAA <30828746+SOMYUUKTHAA@users.noreply.github.com>
2017-09-05 01:06:29 +0530

    Create som27.c

---
+++ som27.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int a,b,Mulp;
+printf("Enter two positive integers: ");
+scanf("%d%d",&a,&b);
+Mulp= (a>b) ?a:b;
+while(1)
+{
+if(Mulp%a==0 && Mulp%b==0)
+{
+printf("The LCM of %d and %d is %d",a,b,Mulp);
+break;
+}
+++Mulp;
+}
+return 0;
+}
